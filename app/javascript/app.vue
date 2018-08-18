d1539c7850a99a22e0aa691d0a5d967bcccd2429
Jooyeon <wndus85@gmail.com>
2018-08-18 22:54:54 +0900

    part-1 카드 보여지는것까지 따라해봄.

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,26 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-  </div>
+    <div id="app" class="row">
+        <div v-for="list in original_lists" class="col-3">
+            <h6> {{list.name}}</h6>
+            <hr/>
+
+            <div v-for="(card, index) in list.cards" class="card card-body">
+                {{card.name}}
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
     export default {
-        data: function () {
-            return {
-                message: "Hello Vue!"
-            }
-        }
+        props: ["original_lists"],
     }
 </script>
 
 <style scoped>
-  p {
-    font-size: 2em;
-    text-align: center;
-  }
+    p {
+        font-size: 2em;
+        text-align: center;
+    }
 </style>
