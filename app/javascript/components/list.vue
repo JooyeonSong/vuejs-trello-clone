1998ebecf02cb25ed4d37577b4db14a844321151
Jooyeon <wndus85@gmail.com>
2018-08-19 16:42:11 +0900

    part-6 list.vue 로 나눔.

--- app/javascript/components/list.vue
+++ app/javascript/components/list.vue
@@ -1,87 +1,92 @@
 <template>
-  <div class="list">
-    <h6>{{ list.name }}</h6>
-
-    <draggable v-model="list.cards" :options="{group: 'cards'}" class="dragArea" @change="cardMoved">
-      <card v-for="card in list.cards" :key="card.id" :card="card" :list="list"></card>
-    </draggable>
-
-    <a v-if="!editing" v-on:click="startEditing">Add a card</a>
-    <textarea v-if="editing" ref="message" v-model="message" class="form-control mb-1"></textarea>
-    <button v-if="editing" v-on:click="createCard" class="btn btn-secondary">Add</button>
-    <a v-if="editing" v-on:click="editing=false">Cancel</a>
-  </div>
+    <div class="list">
+        <h6> {{list.name}}</h6>
+        <draggable v-model="list.cards" :options="{group: 'cards'}" class="dragArea" @change="cardMoved">
+            <div v-for="(card, index) in list.cards" class="card card-body mb-3">
+                {{card.name}}
+            </div>
+        </draggable>
+        <a v-if="editing">Add a card</a>
+        <textarea v-model="messages" class="form-control mb-1"></textarea>
+        <button v-on:click="submitMessages" class="btn btn-secondary">Add</button>
+    </div>
 </template>
 
+
 <script>
-import draggable from 'vuedraggable'
-import card from 'components/card'
+    import draggable from 'vuedraggable'
 
-export default {
-  components: { card, draggable },
-  props: ["list"],
+    export default {
+        components: {draggable},
+        props: ["list"],
 
-  data: function() {
-    return {
-      editing: false,
-      message: ""
-    }
-  },
+        data: function () {
+            return {
+                messages: ""
+            }
+        },
 
-  methods: {
-    startEditing: function() {
-      this.editing = true
-      this.$nextTick(() => { this.$refs.message.focus() })
-    },
+        methods: {
+            cardMoved: function (event) {
+                const evt = event.added || event.moved
+                if (evt == undefined) {
+                    return
+                }
+                const element = evt.element
 
-    cardMoved: function(event) {
-      const evt = event.added || event.moved
-      if (evt == undefined) { return }
+                const list_index = window.store.lists.findIndex((list) => {
+                    return list.cards.find((card) => {
+                        return card.id === element.id
+                    })
+                })
 
-      const element = evt.element
-      const list_index = window.store.state.lists.findIndex((list) => {
-        return list.cards.find((card) => {
-          return card.id === element.id
-        })
-      })
+                var data = new FormData
+                data.append("card[list_id]", window.store.lists[list_index].id)
+                data.append("card[position]", evt.newIndex + 1)
 
-      var data = new FormData
-      data.append("card[list_id]", window.store.state.lists[list_index].id)
-      data.append("card[position]", evt.newIndex + 1)
+                Rails.ajax({
+                    beforeSend: () => true,
+                    url: `/cards/${element.id}/move`,
+                    type: "PATCH",
+                    data: data,
+                    dataType: "json"
+                })
+            },
+            submitMessages: function () {
+                var data = new FormData
+                data.append("card[list_id]", this.list.id)
+                data.append("card[name]", this.messages)
 
-      Rails.ajax({
-        beforeSend: () => true,
-        url: `/cards/${element.id}/move`,
-        type: "PATCH",
-        data: data,
-        dataType: "json"
-      })
-    },
+                Rails.ajax({
+                    beforeSend: () => true,
+                    url: "/cards",
+                    type: "POST",
+                    data: data,
+                    dataType: "json",
+                    success: (data) => {
+                        const index = window.store.lists.findIndex(item => item.id == this.list.id);
+                        window.store.lists[index].cards.push(data);
 
-    createCard: function() {
-      var data = new FormData
-      data.append("card[list_id]", this.list.id)
-      data.append("card[name]", this.message)
-
-      Rails.ajax({
-        beforeSend: () => true,
-        url: "/cards",
-        type: "POST",
-        data: data,
-        dataType: "json",
-        success: (data) => {
-          this.message = ""
-          this.$nextTick(() => { this.$refs.message.focus() })
+                        this.messages = "";
+                    }
+                });
+            }
         }
-      })
     }
-  }
-}
 </script>
-
 <style scoped>
-.dragArea {
-  min-height: 10px;
-}
+    .dragArea {
+        min-height: 10px;
+    }
+
+    .list {
+        display: inline-block;
+        border-radius: 3px;
+        padding: 20px;
+        background: #E2E4E6;
+        width: 270px;
+        vertical-align: top;
+        margin-right: 56px;
+    }
 
 </style>
